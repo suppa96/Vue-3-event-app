a6daa50b8537e4d24e46ae1a6897af0c4c5fe924
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2023-04-07 14:41:11 +0200

    feat: add Details page

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { EventService } from '@/services/EventService.js'
+const event = ref(null) //abbiamo questo evento che faremo vedere utilizzando un metodo che recupera un evento solo dall'api
+
+onMounted(() => {
+  //quando viene mountata l'app facciamo la nostra chiamata API Axios
+  EventService.getEvent(id).then(
+    ((response) => {
+      event.value = response.data
+    }).catch((error) => console.log(error))
+  )
+})
+</script>
+
+<style lang="scss" scoped></style>
